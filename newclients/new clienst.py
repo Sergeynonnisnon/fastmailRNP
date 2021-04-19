9c9c36de8528d93969fe2289a7d4d0e3bffd0dc4
Serg <goszzakupki@yandex.ru>
2021-04-19 22:32:45 +0300

    sintax sugar

--- newclients/new clienst.py
+++ newclients/new clienst.py
@@ -4,8 +4,6 @@ import requests
 from bs4 import BeautifulSoup
 from UA import ua
 import sqlite3
-import pprint
-from db import base
 
 """
 """
@@ -15,7 +13,8 @@ class new_clients():
     def __init__(self):
         pass
 
-    def get_new_clients_href(self, lens=100, number_list=1):
+    @staticmethod
+    def get_new_clients_href(lens=100, number_list=1):
         """
         :param lens: len records on page in url search
         :param number_list: pagination number list
@@ -41,34 +40,32 @@ class new_clients():
               '&countryRegIdNameHidden=%7B"1268"%3A"РОССИЯ"+%7D' \
               '&smallOrMiddle=on'
 
-
         response = requests.get(url, headers={'accept': '*/*', 'user-agent': ua.firefox})
         if response.status_code > 200:
             print('requests bad')
-
 
         soup = BeautifulSoup(response.text, 'lxml')
         soup = soup.body
 
         quotes = soup.find_all('div', class_="registry-entry__header-mid__number")
 
-        contract_number = {}
-        for i in quotes:
-            href = i.find('a')
+        for hrefs in quotes:
+            href = hrefs.find('a')
             href = href.get('href')
 
             result.append(href)
 
         return result
 
-    def new_clients_full_info(self, hrefs):
+    @staticmethod
+    def new_clients_full_info(hrefs):
         """
         :param hrefs: list /epz/eruz/card/general-information.html?reestrNumber=21034689
         :return:
         """
-        result=[]
+        result = []
         for href in hrefs:
-            url = 'https://zakupki.gov.ru'+href
+            url = 'https://zakupki.gov.ru' + href
             print(url)
             response = requests.get(url, headers={'accept': '*/*', 'user-agent': ua.firefox})
             if response.status_code > 200:
@@ -77,11 +74,11 @@ class new_clients():
             soup = soup.body
             quotes = soup.find_all('section', class_="blockInfo__section section")
             for tag in quotes:
-                y = tag.find('span',class_='section__title').text
-                if y=='Тип участника закупки':
-                    type_org = tag.find('span',class_='section__info').text
-                if y=='ФИО':
-                    FIO = tag.find('span',class_='section__info').text
+                y = tag.find('span', class_='section__title').text
+                if y == 'Тип участника закупки':
+                    type_org = tag.find('span', class_='section__info').text
+                if y == 'ФИО':
+                    FIO = tag.find('span', class_='section__info').text
                 if y == 'ИНН':
                     INN = tag.find('span', class_='section__info').text
                 if y == 'Адрес электронной почты':
@@ -103,33 +100,30 @@ class new_clients():
             try:
                 phone
             except UnboundLocalError:
-                phone=None
-            result.append((name,type_org,FIO,INN,email,phone,0))
+                phone = None
+            result.append((name, type_org, FIO, INN, email, phone, 0))
 
-            name, type_org, FIO, INN, email, phone = None ,None,None,None,None,None
+            name, type_org, FIO, INN, email, phone = None, None, None, None, None, None
         return result
 
-
-    def upgrade_newclients_db(self,info):
+    def upgrade_newclients_db(self, info):
         con = sqlite3.connect(f'newclients.db')
         cur = con.cursor()
         for record in info:
-            cur.execute(f'''SELECT * FROM newclients WHERE INN=?''', (record [3],))
+            cur.execute(f'''SELECT * FROM newclients WHERE INN=?''', (record[3],))
             check = cur.fetchall()
             if check == []:
-
-                cur.execute(f'INSERT INTO newclients VALUES(?,?,?,?,?,?,?)',record)
+                cur.execute(f'INSERT INTO newclients VALUES(?,?,?,?,?,?,?)', record)
                 print(record, f'append in newclients ')
             else:
                 print(f'alredy in newclients')
         con.commit()
         con.close()
 
+
 new_clients = new_clients()
-base().bd_create(name='newclients', namecol='name text, type_org text, FIO text, INN text, email text, phone text, status email integer')
+# base().bd_create(name='newclients', namecol='name text, type_org text, FIO text, INN text, email text, phone text, status email integer')
 for i in range(1, 5):
-    a=new_clients.get_new_clients_href(lens=100, number_list=i)
-    info=new_clients.new_clients_full_info(a)
+    a = new_clients.get_new_clients_href(lens=100, number_list=i)
+    info = new_clients.new_clients_full_info(a)
     new_clients.upgrade_newclients_db(info)
-
-
