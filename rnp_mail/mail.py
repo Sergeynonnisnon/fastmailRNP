478e7b97036d2f2d21715e07b678be4cd508df09
Serg <goszzakupki@yandex.ru>
2021-05-11 12:56:11 +0300

    fix coments

---
+++ rnp_mail/mail.py
@@ -0,0 +1,98 @@
+# -*- coding: UTF-8 -*-
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+import smtplib
+import sqlite3
+import time
+import random
+
+
+# create message object instance
+def send_email(mess, to_email, subscription):
+    msg = MIMEMultipart()
+
+    message = f'''Добрый день, Заказчик принял решение расторгнуть с Вами контракт в одностороннем порядке.
+
+Мы знаем, что и в какие сроки необходимо сделать, чтобы Заказчик отменил данное решение и сведения о Вас не были включены в РНП, а также, как вернуть обеспечение контракта. Вы можете задать любые вопросы!
+
+Для этого просто позвоните по телефону +7 978 721 5775 или напишите нам.
+Если своими силами не получитьcя урегулировать данную проблему, пишите нам. Мы на протяжении 5 лет решаем подобные вопросы, я и мои коллеги, опытные юристы, исходя из документов по контракту, заранее можем сказать, что Вас ожидает.
+Если дело провальное, то мы так и сообщаем клиенту, так как наша мотивация на прямую зависит от победы. Попадание в РНП приводит к большим проблемам, чем простой запрет на участие в закупке, в том числе проблемам с банками, с текущими обязательствами и прочее. И часто действует как отталкивающий фактор (наподобие решения о блокировке счета ФНС). Ваша информация сразу показывается во всех общедоступных источниках по проверке контрагентов, более того, если Вы один раз там побывали, то эта информация останется в отношении вашей компании навсегда.
+
+Обращаем Ваше внимание, что согласно ч.6 ст.104 ФЗ-44, Заказчик в течение 3 рабочих дней направляет в УФАС, информацию с обоснованием причин одностороннего отказа. После этого, УФАС осуществляет проверку этих сведений в течение 5 рабочих дней согласно ч.7 ст.104 ФЗ-44. В случае подтверждения достоверности фактов, указанных Заказчиком, УФАС включает информацию (ИНН организации, учредителей и Директора) в реестр недобросовестных поставщиков (РНП) в течение 3 рабочих дней.По вопросам помощи в решении данной ситуации, а также по любым вопросам, связанным с 44 и 223 ФЗ обращайтесь по указанным ниже контактам:
+
+Cсылка для ознакомления:
+https://zakupki.gov.ru/epz/dizk/dizkCard/generalInformation.html?dizkId={mess}
+
+г. Киров, ул. Калинина, 38,
+офис 313.
+тел. + 79787215775
+'''
+
+    # setup the parameters of the message
+    password = "Wertu007!"
+    msg['From'] = "rnp.informator@gmail.com"
+    msg['To'] = to_email
+    msg['Subject'] = f'уведомление о внесении в РНП по контракту№{subscription}'
+
+    # add in the message body
+    msg.attach(MIMEText(message, 'plain'))
+
+    # create server
+    server = smtplib.SMTP('smtp.gmail.com: 587')
+
+    server.starttls()
+
+    # Login Credentials for sending the mail
+    server.login(msg['From'], password)
+
+    # send the message via the server.
+    server.sendmail(msg['From'], msg['To'], msg.as_string())
+
+    server.quit()
+
+    con = sqlite3.connect('RNP.db')
+    cur = con.cursor()
+    cur.execute(f"UPDATE RNP SET status = 0 WHERE  decision_number = {mess}")
+    con.commit()
+    con.close()
+def format_string(a):
+    for i in a:
+        if i == '.':
+            a = a.split('.')
+            x = str()
+            for i in a:
+                if i != a[-1]:
+                    x += i + '\.'
+                else:
+                    x += i
+    return x
+
+def mailing():
+    con = sqlite3.connect('RNP.db')
+    cur = con.cursor()
+    cur.execute('''SELECT * FROM RNP WHERE status = 1 AND email_status = 1''')
+    exists = cur.fetchall()
+    for data in exists:
+        #format_email = format_string(str(data[2]))
+        cur.execute(f'''SELECT * FROM RNP WHERE email = ?''',(data[2],))
+        checks = cur.fetchall()
+
+        if len(checks) <= 1:
+            send_email(data[0], data[2], data[1])
+            rand_sleep = random.randint(1, 5)
+            time.sleep(float(rand_sleep))
+            print(f'отправлено на {data[2]}')
+            print(f'Ожидаю {rand_sleep} секунд для отправки следующего письма')
+
+        else:
+            cur.execute(f"UPDATE RNP SET status = 0 WHERE  email = ?",(data[2],))
+            con.commit()
+            print(f'{checks} уже в таблице и ему не будет направленн емейл')
+
+
+    # cur.execute(f"UPDATE RNP SET status = 0 WHERE  decision_number = {mess}")
+    print(f'направлено {len(exists)} емейл')
+    con.commit()
+    con.close()
+
---
+++ rnp_mail/main.py
@@ -0,0 +1,174 @@
+import re
+import time
+import sys
+import requests
+from bs4 import BeautifulSoup
+from fake_useragent import UserAgent
+
+from mail import *
+
+ua = UserAgent()
+
+
+
+
+def get_href(lens,number_list):
+    """
+
+    :return: lits 'decision_number'
+    """
+    print(f"вы выбрали спарсить страницу № {number_list} , дождитесь окончания работы скрипта")
+    url = 'https://zakupki.gov.ru/epz/dizk/search/results.html?searchString=&morphology=on&search-filter=Дате+размещения' \
+          f'&pageNumber={number_list}&sortDirection=false&recordsPerPage=_{lens}&showLotsInfoHidden=false&savedSearchSettingsIdHidden=' \
+          '&sortBy=UPDATE_DATE&published=on&ur=on&fg=on&fs=on&customerIdOrg=&customerFz94id=&customerTitle=&agencyIdOrg=' \
+          '&customerFz94id=&customerTitle=&customerPlace=&customerPlaceCodes=&registerContractNumber=&contractNumber=&order' \
+          'Number=&orderName=&placingWayList=&selectedLaws=&supplier=&inn=&kpp=&contractDateFrom=&contractDateTo=&publishDateFrom' \
+          '=&publishDateTo=&updateDateFrom=&updateDateTo=&searchTextInAttachedFile='
+
+    response = requests.get(url)
+    soup = BeautifulSoup(response.text, 'lxml')
+    quotes = soup.find_all('div', class_='registry-entry__header-mid__number')
+    result = []
+    for elem in quotes:
+        m = re.search(r'\d{6}', str(elem))
+        result.append(m.group())
+
+    return result
+
+
+def number_contract(numbers):
+    """
+
+    :param numbers: list of 'decision_number'
+    :return: dict {'decision_number' :'contract_number'}
+    """
+    answers = {}
+    for i in numbers:
+
+        url = f'https://zakupki.gov.ru/epz/dizk/dizkCard/generalInformation.html?dizkId={i}'
+        response = requests.get(url)
+        soup = BeautifulSoup(response.text, 'lxml')
+        quotes = soup.find_all('span', class_='section__info', limit=4)
+        for elem in quotes:
+            m = re.search(r'\d{19}', str(elem))
+            try:
+
+                answers[i] = m.group()
+            except:
+                answers[i] = None
+
+    print(answers)
+    return answers
+
+
+def response(answers):
+    for i in answers:
+        urls = 'https://zakupki.gov.ru/epz/contract/contractCard/common-info.html?reestrNumber='
+        url = urls + str(answers[i])
+        if url is not None:
+            response = requests.get(url, headers={'accept': '*/*', 'user-agent': ua.firefox})
+        else:
+            continue
+
+        if response.status_code == 404:
+            time.sleep(2)
+            print('404')
+
+
+def get_email(answers):
+    """
+    :param answers: gives dict numbers contacts like { '147046': '2272291208821000036'}}
+    :return: dict {'decision_number': 'contract_number ','email',...}
+    """
+    a = 0
+    for i in answers:
+        a += 1
+        urls = 'https://zakupki.gov.ru/epz/contract/contractCard/common-info.html?reestrNumber='
+        url = urls + str(answers[i])
+        time.sleep(1)
+        if url is not None:
+            response = requests.get(url, headers={'accept': '*/*', 'user-agent': ua.firefox})
+
+        else:
+
+            continue
+
+        if response.status_code == 404:
+            time.sleep(2)
+            print('ошибка 404')
+        print(f'загружено {a} ссылок')
+        soup = BeautifulSoup(response.text, 'lxml')
+        quotes = soup.find_all('div', class_='container')
+
+        try:
+            m = re.search(r'[\w+\.-]*\w+@[\w+\.]*\w+[\w+\-\w+]*\.\w+', str(quotes))
+            answers[i] = [answers[i], m.group()]
+        except:
+            answers[i] = [answers[i], None]
+            continue
+    return answers
+
+
+def bd_create():
+    """create db on base path
+    :return: bd RNP with colums where :
+    decision_number - number in link of desision to send RNP
+    contract_number- contract number in link
+    email -email in scrap contract,
+    status - 1/0 1- no send email 0 - email send
+    email_status- 1- email exist 0- email dont exist"""
+    con = sqlite3.connect('RNP.db')
+    cur = con.cursor()
+    cur.execute('''CREATE TABLE RNP
+                   (decision_number text,  contract_number text, email text, 
+                    status real,email_status)''')
+    con.commit()
+    con.close()
+
+
+def upgrade_row(answers):
+    """
+    upgrade the db with rule - if desision_number in db= dont append row,
+    else - append db answers and check have row a email
+    :param answers: dict like 'decision_number': 'contract_number ','email'
+    :return: None
+    """
+    con = sqlite3.connect('RNP.db')
+    cur = con.cursor()
+
+    for i in answers:
+
+        if answers[i][1] is not None:
+            add = [i, answers[i][0], answers[i][1], 1, 1]
+        else:
+            add = [i, answers[i][0], answers[i][1], 1, 0]
+
+        cur.execute(f'''SELECT decision_number FROM RNP WHERE decision_number={i}''')
+        exists = cur.fetchall()
+        if not exists:
+            cur.execute('INSERT INTO RNP VALUES(?,?,?,?,?)', add)
+            con.commit()
+        else:
+            print(f"url alredy in table.{add}")
+
+            continue
+
+    con.commit()
+    con.close()
+
+
+def main():
+    for i in range(1,15):
+        number_list=i
+        lens = 100
+        href = get_href(lens, number_list)
+        number = number_contract(href)
+        email = get_email(number)
+        upgrade_row(email)
+        mailing()
+
+
+
+
+if __name__ == '__main__':
+    main()
